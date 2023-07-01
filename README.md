7b8a921d363e6eabe9f66e47b657d2380121fa95
Ehsan <eh.etesami@gmail.com>
2023-07-01 04:42:58 +0000

    Run kubebuilder create webhook command

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 kubebuilder init --domain samples-k8s-ctrl.github.com  --repo github.com/etesami/sample-k8s-ctrl
 kubebuilder create api --group calculator --version v1alpha1 --kind Calculator
 ```
+
+## Implementing a Validating Webhook
+```bash
+kubebuilder create webhook --group calculator --version v1alpha1 --kind Calculator --defaulting --programmatic-validation
+```
--- cmd/main.go
+++ cmd/main.go
@@ -96,6 +96,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Calculator")
 		os.Exit(1)
 	}
+	if err = (&calculatorv1alpha1.Calculator{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "Calculator")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
