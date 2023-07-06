2c52552c739ef8b3a9a29a0198baf849c49359e6
Ehsan Etesami <eh.etesami@gmail.com>
2023-07-06 05:40:19 +0000

    Add SimpleDeployment resource with reference to a deployment

--- api/test1/v1alpha1/simpledeployment_types.go
+++ api/test1/v1alpha1/simpledeployment_types.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1alpha1
 
 import (
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -28,8 +29,14 @@ type SimpleDeploymentSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of SimpleDeployment. Edit simpledeployment_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	Replicas   *int32                `json:"replicas,omitempty"`
+	Selector   *metav1.LabelSelector `json:"selector,omitempty"`
+	Template   *v1.PodTemplateSpec   `json:"template,omitempty"`
+	MyMetadata *MyMetadataSpec       `json:"mymetadata,omitempty"`
+}
+
+type MyMetadataSpec struct {
+	Labels map[string]string `json:"labels"`
 }
 
 // SimpleDeploymentStatus defines the observed state of SimpleDeployment
--- internal/controller/test1/simpledeployment_controller.go
+++ internal/controller/test1/simpledeployment_controller.go
@@ -19,23 +19,33 @@ package test1
 import (
 	"context"
 
+	kapps "k8s.io/api/apps/v1"
+
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/klog/v2"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	test1v1alpha1 "github.com/etesami/sample-k8s-ctrl/api/test1/v1alpha1"
+	"github.com/go-logr/logr"
 )
 
 // SimpleDeploymentReconciler reconciles a SimpleDeployment object
 type SimpleDeploymentReconciler struct {
 	client.Client
+	Log    logr.Logger
 	Scheme *runtime.Scheme
 }
 
 //+kubebuilder:rbac:groups=test1.samples-k8s-ctrl.github.com,resources=simpledeployments,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=test1.samples-k8s-ctrl.github.com,resources=simpledeployments/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=test1.samples-k8s-ctrl.github.com,resources=simpledeployments/finalizers,verbs=update
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=apps,resources=deployments/status,verbs=get
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -50,13 +60,73 @@ func (r *SimpleDeploymentReconciler) Reconcile(ctx context.Context, req ctrl.Req
 	_ = log.FromContext(ctx)
 
 	// TODO(user): your logic here
+	log := r.Log.WithValues("simpleDeployment", req.NamespacedName)
 
-	return ctrl.Result{}, nil
+	var simpleDeployment test1v1alpha1.SimpleDeployment
+	klog.Infof("Fetching the simpledeployment")
+	if err := r.Get(ctx, req.NamespacedName, &simpleDeployment); err != nil {
+		log.Error(err, "unable to fetch SimpleDeployment")
+		// we'll ignore not-found errors, since they can't be fixed by an immediate
+		// requeue (we'll need to wait for a new notification), and we can get them
+		// on deleted requests.
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	deployment := &kapps.Deployment{}
+	// Set the information you care about
+	deployment.ObjectMeta.Namespace = simpleDeployment.Namespace
+	deployment.ObjectMeta.Name = simpleDeployment.Name
+	deployment.Spec.Replicas = simpleDeployment.Spec.Replicas
+	deployment.Spec.Template = *simpleDeployment.Spec.Template
+	deployment.Spec.Selector = simpleDeployment.Spec.Selector
+	deployment.Spec.Template.ObjectMeta.Labels = simpleDeployment.Spec.MyMetadata.Labels
+
+	// var labelSelector v1.LabelSelector
+	// mapString := map[string]interface{}{
+	// 	"matchLabels": map[string]string{
+	// 		"app": "nginx",
+	// 	},
+	// }
+	// // Convert the nested map to a map[string]string
+	// matchLabels, ok := mapString["matchLabels"].(map[string]string)
+	// if !ok {
+	// 	fmt.Println("Error: Invalid structure1")
+	// 	return ctrl.Result{}, nil
+	// }
+	// if err := v1.Convert_Map_string_To_string_To_v1_LabelSelector(&matchLabels, &labelSelector, nil); err != nil {
+	// 	log.Error(err, "unable to convert jsonString to labelSelector: matchLabels")
+	// 	return ctrl.Result{}, err
+	// } else {
+	// 	deployment.Spec.Selector = &labelSelector
+	// }
+
+	klog.Infof("Calling SetControllerReference")
+	if err := controllerutil.SetControllerReference(&simpleDeployment, deployment, r.Scheme); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	foundDeployment := &kapps.Deployment{}
+	err := r.Get(ctx, types.NamespacedName{Name: deployment.Name, Namespace: deployment.Namespace}, foundDeployment)
+	if err != nil && errors.IsNotFound(err) {
+		klog.Infof("Creating Deployment", "deployment", deployment.Name)
+		err = r.Create(ctx, deployment)
+	} else if err == nil {
+		if foundDeployment.Spec.Replicas != deployment.Spec.Replicas {
+			foundDeployment.Spec.Replicas = deployment.Spec.Replicas
+			klog.Info("Updating Deployment", "deployment", deployment.Name)
+			err = r.Update(ctx, foundDeployment)
+		}
+	} else {
+		klog.Error("88888888888888 Something else happened")
+	}
+
+	return ctrl.Result{}, err
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *SimpleDeploymentReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&test1v1alpha1.SimpleDeployment{}).
+		// Owns(&kapps.Deployment{}).
 		Complete(r)
 }
