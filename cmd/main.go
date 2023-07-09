cc2d69f7ef0fbfa56777bd4f7c3e3606919b64f8
Ehsan Etesami <eh.etesami@gmail.com>
2023-07-09 21:40:08 +0000

    WIP: ConfigDeployment (update failes)

--- cmd/main.go
+++ cmd/main.go
@@ -110,6 +110,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "SimpleDeployment")
 		os.Exit(1)
 	}
+	if err = (&test1controller.ConfigDeploymentReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ConfigDeployment")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
